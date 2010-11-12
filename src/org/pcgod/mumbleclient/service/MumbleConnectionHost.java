57b57cade4992ed17b2fe8101c564ed2dd2426a0
Mikko Rantanen <jubjub@jubjubnest.net>
2010-11-12 20:41:43 +0200

    Removed "Disconnecting" state. When 'connected' state is lost, Connection shouldn't be used anyway no matter whether it's in 'disconnecting' or 'disconnected' state.

--- src/org/pcgod/mumbleclient/service/MumbleConnectionHost.java
+++ src/org/pcgod/mumbleclient/service/MumbleConnectionHost.java
@@ -2,15 +2,14 @@ package org.pcgod.mumbleclient.service;
 
 /**
  * Callback interface for Connection to communicate back to the service.
- * 
+ *
  * @author Rantanen
- * 
+ *
  */
 public interface MumbleConnectionHost {
 	public final static int STATE_DISCONNECTED = 0;
 	public final static int STATE_CONNECTING = 1;
 	public final static int STATE_CONNECTED = 2;
-	public final static int STATE_DISCONNECTING = 3;
 
 	public void setConnectionState(int state);
 
